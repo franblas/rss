bc8509e64d6736029921c9cb7c863f7346291931
franblas <francois_blas@hotmail.com>
2025-06-14 14:57:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function parseItem(item, isFeed) {
 }
 
 function displayItems(fluxId, rssurl, color) {
-  var corsProxy = "https://corsproxy.io/"
+  var corsProxy = "https://corsproxy.io/?url="
   var url = corsProxy + rssurl
   $.get(url, function(data) {
       var $xml = $(data)
